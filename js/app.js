0308e8561b056deb81068e41f523286b99353388
ruthmz <rmatosfer@gmail.com>
2017-11-17 04:59:26 -0500

    version 0.0.2

--- js/app.js
+++ js/app.js
@@ -1,19 +1,43 @@
 
 var submitButton = document.getElementById('submit');
-function submitClicked () {
-    var message = document.getElementById('message-input').value;
-    // var text= document.createElement('p')
-    // var texto = document.createTextNode(message);
-    // var result =  document.getElementById("container").appendChild(message);
-    document.getElementById('container').innerHTML = message;
+var wholeMessage = document.getElementById('message-input');
+
+function submitClicked (event) {
+    var message = wholeMessage.value;    
+    var element = document.createElement('div');
+    element.appendChild(document.createTextNode(message));  
+    document.getElementById('container').appendChild(element);
+    element.setAttribute('class','box')
     clearMessage ();
+    enabled ();
 }
+
+function enabled (event) {
+    if(wholeMessage.value == ''){
+        document.getElementById('submit').disabled = true;
+    } else {
+        document.getElementById('submit').disabled = false;       
+    }
+}
+setInterval( "enabled (event)",100)
+
+function counterChar (event) {
+    var text = wholeMessage.value;
+    var number = document.getElementById('number');
+    var counter = parseInt((140-(text.length)));
+    number.innerHTML = counter; 
+}
+wholeMessage.addEventListener('keyup',counterChar, false);
+
+
 // submitButton.onclick = submitClicked;
 submitButton.addEventListener('click',submitClicked);
 
 function clearMessage () {
     document.getElementById('message-input').value = '';
 }
+
+
 
 
 /*
