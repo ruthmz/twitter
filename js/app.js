062549d4795892fd7f5c980ce38a94b600366c1f
ruthmz <rmatosfer@gmail.com>
2017-11-18 17:38:08 -0500

    version 0.0.6

--- js/app.js
+++ js/app.js
@@ -2,23 +2,34 @@
 var submitButton = document.getElementById('submit');
 var wholeMessage = document.getElementById('message-input');
 var number = document.getElementById('number');
+var container = document.getElementById('container');
+var currentTime = new Date();
+var hour = currentTime.getHours()+ ':' + currentTime.getMinutes();
+
 
 //Creamos una funcion para que cree un div que almacene los mensajes del input
 function submitClicked (event) {
     var message = wholeMessage.value;    
     var element = document.createElement('div');
+    var newHour = document.createElement('div');
+    newHour.innerHTML = hour;
+    element.appendChild(newHour);
     element.appendChild(document.createTextNode(message));  
-    document.getElementById('container').appendChild(element);
+    container.appendChild(element);
     element.setAttribute('class', 'box');
     number.textContent=140;  
     clearMessage ();
     enabled ();
 }
+submitButton.addEventListener('click',submitClicked);
+  
 // Habilita el botón, solo si se ingresa texto y la cantidad de letras es menor a 141
 function enabled (event) {
     if(wholeMessage.value == ''){
         document.getElementById('submit').disabled = true;
     } else if (wholeMessage.value.length>=141) {
+        document.getElementById('submit').disabled = true;
+    }  else if (wholeMessage.value == '' || wholeMessage == "\n"){
         document.getElementById('submit').disabled = true;
     }
     else {
@@ -31,7 +42,7 @@ setInterval( "enabled (event)",100)
 function counterChar (event) {
     var text = wholeMessage.value;
     var counter = parseInt((140-(text.length)));
-    if(text.length>=140){
+    if(text.length>140){
        document.getElementById('number').style.color = "red";
     } else if(text.length>=130 && text.length <= 140){
         document.getElementById('number').style.color = "gray";
@@ -40,14 +51,23 @@ function counterChar (event) {
     }
     number.innerHTML = counter; 
 }
-wholeMessage.addEventListener('keyup',counterChar);
+
+function scrooll(event){
+    wholeMessage.style.height = 'inherit'
+    wholeMessage.style.height = wholeMessage.scrollHeight + "px";
+  }
+
+wholeMessage.addEventListener('input',scrooll);
+
+// wholeMessage.addEventListener('keyup',counterChar);
+
 
 // submitButton.onclick = submitClicked;
-submitButton.addEventListener('click',submitClicked);
 
 function clearMessage () {
     document.getElementById('message-input').value = '';
 }
+
 
 
 
